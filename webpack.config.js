e46163b1730d0d840e3b3c59aa96056404339596
totw5701 <76473868+totw5701@users.noreply.github.com>
2021-02-06 01:41:37 +0900

    #webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const ExtractCss = require("extract-text-webpack-plugin");
+
+const MODE = process.env.WEBPACK_ENV;
+const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
+const OUTPUT_DIR = path.join(__dirname, "static");
+
+const config = {
+    entry: ENTRY_FILE,
+    mode: MODE,
+    module: {
+        rules: [
+            {
+                test: /\.(scss)$/,
+                use: ExtractCss.extract([
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: "postcss-loader"
+                    },
+                    {
+                        loader: "sass-loader"
+                    }
+                ])
+            }
+        ]
+    },
+    output: {
+        path: OUTPUT_DIR,
+        filename: "[name].[format]"
+    }
+};
+
+module.exports = config;
