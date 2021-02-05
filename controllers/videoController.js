4981ee2089182d06222c97c5351530dd53c9ef70
totw5701 <76473868+totw5701@users.noreply.github.com>
2021-02-05 22:12:00 +0900

    #3 MongoDB done

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -8,8 +8,8 @@ export const home = async(req, res) => {
     // 지금 성공 여부를 체크하는게 중요한 이유는 다음과 같다. Video.find로 모델을 가지고오는데 실패하더라도 videos를 []라는 빈 array로 만들어 시스템은 정상적으로 작동하게 하기 위함. 왜그랬는지는 모르겠음.
 
     try{
-        const videos = await Video.find({});
-        res.render("home", {pageTitle: "Home", videos:videos});
+        const videos = await Video.find({}).sort({_id:-1});   // 정렬을 반대로해서(sort) 새로 올린 동영상에 위에 뜨도록 설정했음. title을 알파벳순으로 등등 자유롭게 정렬할 수 있다.
+        res.render("home", {pageTitle: "Home", videos});
     } catch(error){  //에러가 발생하면 아래를 시작하라라는 뜻
         console.log(error);
         res.render("home", {pageTitle: "Home", videos: []}); // 에러가 생기면 videos를 빈 어레이로 만들어버렸다. 
@@ -35,11 +35,19 @@ export const home = async(req, res) => {
 
 
 
-export const search = (req, res) => {
-    // console.log(req); 이걸로 req 열어서 내가 입력한 값 어디에 있는지 찾아서 아래 처럼 출력해줬음
+export const search = async(req, res) => {
+    // console.log(req); 이걸로 req 열어서 내가 input에 입력한 값 어디에 있는지 찾아서 아래 처럼 출력해줬음
     const {query:{term:searchingBy}} = req; // req.query.term 한거랑 같음.
     //const searchingBy = req.query.term;
-    res.render("search", { pageTitle: "search", searchingBy: searchingBy,videos:videos});
+    
+    let videos = [];
+    try{
+        videos = await Video.find({title: {$regex: searchingBy, $options: "i"}});    //(title:searchingBy) 이렇게 하면 내가 입력한 값과 완벽히 일치하는 것만 찾아온다
+        // 하지만 searchingBy가 포함된 얘들 모두를 찾고싶으면 저렇게 regular express(cntl + f 해서 단어 찾는걸 지칭함)을 활용하면 된다. option i (insensitive)를 추가하면 대소문자를 구분하지 않는다.
+    }catch(error){
+        console.log(error);
+    }
+    res.render("search", { pageTitle: "search", searchingBy, videos });
 
 }
 export const getUpload = (req, res) => res.render("upload", { pageTitle: "upload"});
@@ -68,7 +76,7 @@ export const videoDetail = async(req, res) => {
 
     try{
         const video = await Video.findById(id);   // mongoose를 이용해 id값으로 video DB롤 특정지어 받아온다.
-        res.render("videoDetail", { pageTitle: "videoDetail", video}); // 그렇게 받아온 video를 pug에 보낸다.
+        res.render("videoDetail", { pageTitle:video.title , video}); // 그렇게 받아온 video를 pug에 보낸다.
     } catch(error) {
         res.redirect(routes.home);
     }
@@ -104,7 +112,17 @@ export const postEditVideo = async(req, res) =>{
 
 };
 
-export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "deleteVideo"});
+export const deleteVideo = async(req, res) => {
+    const {
+        params: {id}
+    } = req;
+    try{
+        await Video.findOneAndRemove({_id:id});  // URL에서 id를 가져오고 mongoose를 이용하여 그것을 삭제하였다.    
+    } catch(error) {
+        console.log("there is an error 119")
+    };
+    res.redirect(routes.home);
+};
 
 
 
